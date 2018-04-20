fcc2fb793e3f239056e0b465e140af413c420516
Vardhan <vrdhn0@gmail.com>
2018-04-20 06:20:29 -0530

    gradle stuff.

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+JAVA_HOME=/home/v/sw//jdk-10.0.1
+export JAVA_HOME
+
+run:
+	./gradlew run
---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+
+package net.vardhan.nq;
+
+
+class Main
+{
+
+
+    public static void main (String[] args) {
+	System.out.println("Yeah");
+    }
+}
